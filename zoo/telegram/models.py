e23937ce93957c68071abdec0cc47d15cd237535
Your Name <you@example.com>
2026-06-05 16:29:40 +0300

    tmp

--- zoo/telegram/models.py
+++ zoo/telegram/models.py
@@ -1,8 +1,9 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 class TelegramSettings(models.Model):
     webhook_url = models.URLField()
-    logo = models.FileField()
+    logo = models.ImageField(validators=[FileExtensionValidator(['png', ])])
 
     def __str__(self):
         return self.webhook_url
